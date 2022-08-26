38b707707af0978674e6669e4dc62d1793aafe1a
Sebastianfacundo <Sebastianvera38@gmail.com>
2022-08-26 18:32:03 -0300

    modificacion output

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ var caja_output = document.getElementById("caja_output");
 var boton_encriptar = document.getElementById("btn_encriptar");
 var boton_desencriptar = document.getElementById("btn_desencriptar");
 var boton_copiar = document.getElementById("btn_copiar");
+var mensaje_lupa=document.getElementById("mensaje_lupa");
 boton_encriptar.onclick = encriptar;
 boton_desencriptar.onclick = desencriptar;
 boton_copiar.onclick = copiar;
@@ -29,8 +30,9 @@ function encriptar() {
     texto = texto.replace(/o/gm, "ober");
     texto = texto.replace(/u/gm, "ufat");
     caja_input.value = "";
+    borrarLupa();
     caja_output.value = texto;
-    boton_copiar.style.display="block";
+    boton_copiar.style.display="inline-block";
   }
 }
 
@@ -43,6 +45,7 @@ function desencriptar() {
     texto = texto.replace(/ober/gm, "o");
     texto = texto.replace(/ufat/gm, "u");
     caja_input.value = "";
+   borrarLupa();
     caja_output.value = texto;
     boton_copiar.style.display="block";
   }
@@ -52,4 +55,9 @@ function copiar() {
   caja_output.select();
   navigator.clipboard.writeText(caja_output.value);
   caja_output.value = "Texto copiado";
+}
+
+function borrarLupa(){
+  caja_output.style.backgroundImage="none";
+  mensaje_lupa.style.display="none";
 }
